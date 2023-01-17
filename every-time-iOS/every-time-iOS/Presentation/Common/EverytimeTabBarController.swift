4037d488bb2aa82c9e49e2a86274958b741abb8c
Mini <minnnidev@gmail.com>
2023-01-17 19:55:29 +0900

    [Fix] #3 - 코드 수정

--- every-time-iOS/every-time-iOS/Presentation/Common/EverytimeTabBarController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Common/EverytimeTabBarController.swift
@@ -7,29 +7,7 @@
 
 import UIKit
 
-class EverytimeTabBar: UITabBar {
-
-    // MARK: - Initializer
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
 class EverytimeTabBarController: UITabBarController {
-    
-    // MARK: - Properties
-    
-    var defaultIndex = 0 {
-        didSet {
-            self.selectedIndex = defaultIndex
-        }
-    }
-    private let everyTimeTabBar = EverytimeTabBar()
     
     // MARK: - View Life Cycle
     
@@ -45,37 +23,31 @@ extension EverytimeTabBarController {
     
     private func setUI() {
         view.backgroundColor = .white
-        selectedIndex = defaultIndex
-        setValue(everyTimeTabBar, forKey: "tabBar")
-        object_setClass(tabBar, EverytimeTabBar.self)
+        
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = .black
         tabBar.itemPositioning = .centered
         tabBar.clearShadow()
         tabBar.layer.applyShadow(color: .black, alpha: 0.5, x: 0, y: 0, blur: 2)
-    }
-}
-
-// MARK: - UITabBarControllerDelegate
-
-extension EverytimeTabBarController: UITabBarControllerDelegate {
-    
-    // MARK: - View Life Cycle
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
         let homeViewController = HomeViewController()
         let homeNavigationController = UINavigationController(rootViewController: homeViewController)
-        
+        homeNavigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+
         let timetableViewController = TImetableViewController()
         let timetableNavigationController = UINavigationController(rootViewController: timetableViewController)
-        
+        timetableNavigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "tablecells"), selectedImage: UIImage(systemName: "tablecells.fill"))
+
         let menuViewController = MenuViewController()
         let menuNavigationController = UINavigationController(rootViewController: menuViewController)
-        
+        menuNavigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "menucard"), selectedImage: UIImage(systemName: "menucard.fill"))
+
         let notificationViewController = NotificationViewController()
         let notificationNavigationController = UINavigationController(rootViewController: notificationViewController)
-        
+        notificationNavigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "bell"), selectedImage: UIImage(systemName: "bell.fill"))
+
         let campusPickViewController = CampusPickViewController()
+        campusPickViewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "icloud"), selectedImage: UIImage(systemName: "icloud.fill"))
         
         let viewControllers = [
             homeNavigationController,
@@ -85,35 +57,10 @@ extension EverytimeTabBarController: UITabBarControllerDelegate {
             campusPickViewController
         ]
         
-        setViewControllers(viewControllers, animated: true)
-        
-        let tabBar: UITabBar = tabBar
-        tabBar.backgroundColor = .white
-        tabBar.barStyle = UIBarStyle.default
-        tabBar.barTintColor = UIColor.white
-        tabBar.tintColor = .black
-
-        let imageNames = [
-            "house",
-            "tablecells",
-            "menucard",
-            "bell",
-            "icloud"
-        ]
-
-        let imageSelectedNames = [
-            "house.fill",
-            "tablecells.fill",
-            "menucard.fill",
-            "bell.fill",
-            "icloud.fill"
-        ]
-
-        for (index, value) in (tabBar.items?.enumerated())! {
-            let tabBarItem: UITabBarItem = value as UITabBarItem
-
-            tabBarItem.image = UIImage(systemName: imageNames[index])
-            tabBarItem.selectedImage = UIImage(systemName: imageSelectedNames[index])
+        for vc in viewControllers {
+            vc.view.backgroundColor = .white
         }
+
+        setViewControllers(viewControllers, animated: true)
     }
 }
