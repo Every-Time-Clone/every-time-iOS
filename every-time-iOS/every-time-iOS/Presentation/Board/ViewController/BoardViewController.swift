6eeb8f835560410347d69ebf9f76968ce8f7719e
Mini <minnnidev@gmail.com>
2023-01-30 20:21:25 +0900

    [Add] #7 - Board 뷰컨트롤러 추가

--- every-time-iOS/every-time-iOS/Presentation/Board/ViewController/BoardViewController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Board/ViewController/BoardViewController.swift
@@ -7,24 +7,30 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class BoardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
+        setLayout()
+    }
+}
 
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .yellow
+extension BoardViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
     }
-    */
-
 }
