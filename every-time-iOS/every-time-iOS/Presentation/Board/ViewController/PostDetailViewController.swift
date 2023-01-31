728ade7448cb9708e091ab095789083e0fa2b52b
Mini <minnnidev@gmail.com>
2023-01-31 14:57:04 +0900

    [Design] #10 - navigation bar 구현

--- every-time-iOS/every-time-iOS/Presentation/Board/ViewController/PostDetailViewController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Board/ViewController/PostDetailViewController.swift
@@ -12,6 +12,10 @@ import Then
 
 class PostDetailViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    private let titleView = TitleView()
+    
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
@@ -19,6 +23,7 @@ class PostDetailViewController: UIViewController {
         
         setUI()
         setLayout()
+        setNavigationBar()
     }
 }
 
@@ -28,11 +33,56 @@ extension PostDetailViewController {
     
     private func setUI() {
         view.backgroundColor = .white
+        
+        titleView.do { 
+            $0.axis = .vertical
+            $0.distribution = .fillEqually
+            $0.setNavigationBarTitle("자유게시판", "동국대 서울캠")
+        }
     }
     
     // MARK: - Layout Helper
     
     private func setLayout() {
         
+    }
+    
+    // MARK: - Methods
+    
+    private func setNavigationBar() {
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action:  #selector(backButtonDidTap))
+        backButton.tintColor = .black
+        
+        let menuButton = UIBarButtonItem(image: UIImage(systemName: "line.3.horizontal"), style: .plain, target: self, action: #selector(menuButtonDidTap))
+        menuButton.tintColor = .black
+        
+        let alarmButton = UIBarButtonItem(image: UIImage(systemName: "bell.slash"), style: .plain, target: self, action: #selector(alarmButtonDidTap))
+        alarmButton.tintColor = .systemGray4
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.rightBarButtonItems = [menuButton, alarmButton]
+        navigationItem.titleView = titleView
+    }
+    
+    // MARK: - @objc Methods
+    
+    @objc private func backButtonDidTap() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    @objc private func menuButtonDidTap() {
+        print("menu")
+    }
+    
+    @objc private func alarmButtonDidTap(_ button: UIBarButtonItem) {
+        if button.style == .plain {
+            button.image = UIImage(systemName: "bell")
+            button.tintColor = UIColor(r: 199, g: 39, b: 9)
+            button.style = .done
+        } else {
+            button.image = UIImage(systemName: "bell.slash")
+            button.tintColor = .systemGray4
+            button.style = .plain
+        }
     }
 }
