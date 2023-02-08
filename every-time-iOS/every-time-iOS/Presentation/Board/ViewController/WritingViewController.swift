a088bc2f46b764b30e12c3a7ece271de78daa2f3
Mini <minnnidev@gmail.com>
2023-02-08 21:42:50 +0900

    [Add] #13 - 글쓰기 뷰 추가

---
+++ every-time-iOS/every-time-iOS/Presentation/Board/ViewController/WritingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WritingViewController.swift
+//  every-time-iOS
+//
+//  Created by 김민 on 2023/02/08.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class WritingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setLayout()
+    }
+}
+
+extension WritingViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        view.backgroundColor = .yellow
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+    }
+}
