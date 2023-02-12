f007060a06632891f7542e5f7e903a4eac91d5d4
Mini <minnnidev@gmail.com>
2023-02-12 16:07:08 +0900

    [Fix] #18 - 홈 탭바 오류 수정

--- every-time-iOS/every-time-iOS/Presentation/Home/ViewController/HomeViewController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Home/ViewController/HomeViewController.swift
@@ -39,7 +39,8 @@ final class HomeViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.isHidden = true
+        tabBarController?.tabBar.isHidden = false
     }
 }
 
--- every-time-iOS/every-time-iOS/Presentation/Menu/MenuViewController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Menu/MenuViewController.swift
@@ -7,23 +7,32 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+import SnapKit
+import Then
+
+final class MenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUI()
+        setLayout()
+    }
+}
+
+extension MenuViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        view.backgroundColor = .yellow
+        
+        navigationController?.navigationBar.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
     }
-    */
-
 }
