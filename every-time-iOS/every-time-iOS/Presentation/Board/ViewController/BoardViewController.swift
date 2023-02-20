b4e96b2c45eb181b37aaae7269dd3db4b4ec2c84
Mini <minnnidev@gmail.com>
2023-02-20 16:12:10 +0900

    [Design] #19 - 네비게이션바 타이틀 수정

--- every-time-iOS/every-time-iOS/Presentation/Board/ViewController/BoardViewController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Board/ViewController/BoardViewController.swift
@@ -99,21 +99,6 @@ extension BoardViewController {
     
     // MARK: - Methods
     
-    private func setNavigationBar() {
-        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(backButtonDidTap))
-        backButton.tintColor = .black
-        
-        let menuButton = UIBarButtonItem(image: UIImage(systemName: "line.3.horizontal"), style: .plain, target: self, action: #selector(menuButtonDidTap))
-        menuButton.tintColor = .black
-        
-        let searchButton = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: #selector(searchButtonDidTap))
-        searchButton.tintColor = .black
-        
-        navigationItem.leftBarButtonItem = backButton
-        navigationItem.rightBarButtonItems = [menuButton, searchButton]
-        navigationItem.titleView = titleView
-    } 
-    
     private func setDelegate() {
         boardTableView.dataSource = self
         boardTableView.delegate = self
@@ -131,7 +116,22 @@ extension BoardViewController {
     
     // MARK: - @objc Methods
     
-    @objc private func backButtonDidTap() {
+    @objc func setNavigationBar() {
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(backButtonDidTap))
+        backButton.tintColor = .black
+        
+        let menuButton = UIBarButtonItem(image: UIImage(systemName: "line.3.horizontal"), style: .plain, target: self, action: #selector(menuButtonDidTap))
+        menuButton.tintColor = .black
+        
+        let searchButton = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: #selector(searchButtonDidTap))
+        searchButton.tintColor = .black
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.rightBarButtonItems = [menuButton, searchButton]
+        navigationItem.titleView = titleView
+    } 
+    
+    @objc func backButtonDidTap() {
         navigationController?.popViewController(animated: true)
     }
     
--- every-time-iOS/every-time-iOS/Presentation/Menu/ViewController/DetailBoardViewController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Menu/ViewController/DetailBoardViewController.swift
@@ -7,24 +7,33 @@
 
 import UIKit
 
-class DetailBoardViewController: UIViewController {
+import SnapKit
+import Then
 
+class DetailBoardViewController: BoardViewController {
+
+    // MARK: - Initializer
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setNavigationBar()
     }
-    */
+}
 
+extension DetailBoardViewController {
+    // MARK: - Methods
+    
+    @objc override func setNavigationBar() {
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(backButtonDidTap))
+        backButton.tintColor = .black
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.title = "내가 쓴 글"
+    }
 }
