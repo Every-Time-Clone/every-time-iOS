df90584e3ae84b2a4b9c71c5658b50e24e9ae278
Mini <minnnidev@gmail.com>
2023-02-23 19:11:16 +0900

    [Design] #27 - 게시판 옵션 디테일 레이아웃

--- every-time-iOS/every-time-iOS/Presentation/Menu/Cell/TableViewCell/BoardOptionTableViewCell.swift
+++ every-time-iOS/every-time-iOS/Presentation/Menu/Cell/TableViewCell/BoardOptionTableViewCell.swift
@@ -18,6 +18,14 @@ final class BoardOptionTableViewCell: UITableViewCell {
     private let defaultOptionView: UIView = UIView()
     private let photoOptionView: UIView = UIView()
     private let optionStackView: UIStackView = UIStackView()
+    private let defaultOptionStackView: UIStackView = UIStackView()
+    private let defaultOptionLabel: UILabel = UILabel()
+    private let defaultOptionDetailLabel: UILabel = UILabel()
+    private let defaultOptionCheckImage: UIImageView = UIImageView()
+    private let photoOptionLabel: UILabel = UILabel()
+    private let photoOptionDetailLabel: UILabel = UILabel()
+    private let photoOptionCheckImage: UIImageView = UIImageView()
+    private let photoOptionStackView: UIStackView = UIStackView()
     
     // MARK: - Initializer
     
@@ -46,17 +54,53 @@ extension BoardOptionTableViewCell {
             $0.layer.cornerRadius = 10
         }
         
-        defaultOptionView.do {
-            $0.backgroundColor = .black
-        }
-        
-        photoOptionView.do {
-            $0.backgroundColor = .cyan
-        }
-        
         optionStackView.do {
             $0.axis = .vertical
             $0.distribution = .fillEqually
+        }
+        
+        defaultOptionLabel.do { 
+            $0.font = .systemFont(ofSize: 15)
+            $0.text = "기본형"
+        }
+        
+        defaultOptionDetailLabel.do {
+            $0.font = .systemFont(ofSize: 10)
+            $0.textColor = .lightGray
+            $0.text = "여러 글을 빠르게 읽을 수 있는 기본 형식"
+        }
+        
+        defaultOptionStackView.do {
+            $0.axis = .vertical
+            $0.distribution = .fillProportionally
+            $0.spacing = CGFloat(5)
+        }
+        
+        photoOptionLabel.do {
+            $0.font = .systemFont(ofSize: 15)
+            $0.text = "사진형"
+        }
+        
+        photoOptionDetailLabel.do {
+            $0.font = .systemFont(ofSize: 10)
+            $0.textColor = .lightGray
+            $0.text = "글 목록에 사진이 노출되는 형식"
+        }
+        
+        photoOptionStackView.do {
+            $0.axis = .vertical
+            $0.distribution = .fillProportionally
+            $0.spacing = CGFloat(5)
+        }
+        
+        defaultOptionCheckImage.do {
+            $0.image = UIImage(systemName: "checkmark")
+            $0.tintColor = .everytimeRed
+        }
+        
+        photoOptionCheckImage.do {
+            $0.image = UIImage(systemName: "checkmark")
+            $0.tintColor = .everytimeRed
         }
     }
     
@@ -66,6 +110,10 @@ extension BoardOptionTableViewCell {
         contentView.addSubview(containerView)
         containerView.addSubview(optionStackView)
         optionStackView.addArrangedSubviews(defaultOptionView, photoOptionView)
+        defaultOptionStackView.addArrangedSubviews(defaultOptionLabel, defaultOptionDetailLabel)
+        defaultOptionView.addSubviews(defaultOptionStackView, defaultOptionCheckImage)
+        photoOptionStackView.addArrangedSubviews(photoOptionLabel, photoOptionDetailLabel)
+        photoOptionView.addSubviews(photoOptionStackView, photoOptionCheckImage)
         
         containerView.snp.makeConstraints {
             $0.top.equalToSuperview().offset(10)
@@ -79,6 +127,28 @@ extension BoardOptionTableViewCell {
             $0.top.equalToSuperview().offset(10)
             $0.bottom.equalToSuperview().offset(-10)
             $0.leading.trailing.equalToSuperview()
+        }
+        
+        defaultOptionStackView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(15)
+        }
+        
+        photoOptionStackView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(15)
+        }
+        
+        defaultOptionCheckImage.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.width.height.equalTo(15)
+        }
+        
+        photoOptionCheckImage.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.width.height.equalTo(15)
         }
     }
 }
