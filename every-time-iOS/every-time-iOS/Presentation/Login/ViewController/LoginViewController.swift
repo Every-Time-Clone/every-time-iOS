c3c9a7057d85b850bb86a76cb2b6a0f266035081
Mini <minnnidev@gmail.com>
2023-02-27 11:54:21 +0900

    [Feat] #30 - 로그인 시 애니메이션 설정

--- every-time-iOS/every-time-iOS/Presentation/Login/ViewController/LoginViewController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Login/ViewController/LoginViewController.swift
@@ -17,6 +17,10 @@ final class LoginViewController: UIViewController {
     private let everyTimeImageView: UIImageView = UIImageView()
     private let descriptionLabel: UILabel = UILabel()
     private let everyTimeLabel: UILabel = UILabel()
+    private let idTextField: UITextField = UITextField()
+    private let passwordTextField: UITextField = UITextField()
+    private let loginButton: UIButton = UIButton()
+    private let loginStackView: UIStackView = UIStackView()
 
     // MARK: - Initializer
     
@@ -25,6 +29,7 @@ final class LoginViewController: UIViewController {
 
         setUI()
         setLayout()
+        setDelegate()
     }
 }
 
@@ -48,12 +53,36 @@ extension LoginViewController {
             $0.textColor = .everytimeRed
             $0.font = .systemFont(ofSize: 20, weight: .bold)
         }
+        
+        idTextField.do {
+            $0.backgroundColor = .yellow
+            $0.placeholder = "아이디"
+        }
+        
+        passwordTextField.do {
+            $0.backgroundColor = .yellow
+            $0.placeholder = "비밀번호"
+        }
+        
+        loginButton.do {
+            $0.backgroundColor = .everytimeRed
+            $0.setTitle("에브리타임 로그인", for: .normal)
+        }
+        
+        loginStackView.do {
+            $0.axis = .vertical
+            $0.distribution = .fillEqually
+            $0.backgroundColor = .cyan
+            $0.spacing = CGFloat(5)
+        }
     }
     
     // MARK: - Layout Helper
     
     private func setLayout() {
-        view.addSubviews(everyTimeImageView, descriptionLabel, everyTimeLabel)
+        view.addSubviews(everyTimeImageView, descriptionLabel, everyTimeLabel, loginStackView)
+        
+        loginStackView.addArrangedSubviews(idTextField, passwordTextField, loginButton)
         
         everyTimeImageView.snp.makeConstraints {
             $0.top.equalToSuperview().offset(100)
@@ -69,6 +98,34 @@ extension LoginViewController {
         everyTimeLabel.snp.makeConstraints {
             $0.top.equalTo(descriptionLabel.snp.bottom).offset(3)
             $0.centerX.equalTo(descriptionLabel)
+        }
+        
+        loginStackView.snp.makeConstraints {
+            $0.top.equalTo(everyTimeLabel.snp.bottom).offset(50)
+            $0.centerX.equalToSuperview()
+            $0.leading.equalToSuperview().offset(30)
+        }
+    }
+
+    // MARK: - Methods
+    
+    private func setDelegate() {
+        idTextField.delegate = self
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        everyTimeImageView.isHidden = true
+        descriptionLabel.isHidden = true
+        everyTimeLabel.isHidden = true
+        
+        UIView.animate(withDuration: 0.25) {
+            self.loginStackView.snp.updateConstraints {
+                $0.top.equalTo(self.everyTimeLabel.snp.bottom).offset(-100)
+            }
+            self.loginStackView.superview?.layoutIfNeeded()
         }
     }
 }
