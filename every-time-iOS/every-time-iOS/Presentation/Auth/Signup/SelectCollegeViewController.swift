b1c358a621408078bdd796f9d000c0737e83c6e9
Mini <minnnidev@gmail.com>
2023-03-06 11:17:08 +0900

    [Design] #34 - 상단 뷰 레이아웃 구현

--- every-time-iOS/every-time-iOS/Presentation/Auth/Signup/SelectCollegeViewController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Auth/Signup/SelectCollegeViewController.swift
@@ -12,6 +12,12 @@ import Then
 
 class SelectCollegeViewController: UIViewController {
     
+    // MARK: - UI Components
+    
+    private let topView: UIView = UIView()
+    private let topTitleLabel: UILabel = UILabel()
+    private let closeButton: UIButton = UIButton()
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
@@ -27,12 +33,41 @@ extension SelectCollegeViewController {
     // MARK: - UI Components Property
     
     private func setUI() {
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        
+        topView.do {
+            $0.backgroundColor = .white
+        }
+        
+        topTitleLabel.do {
+            $0.text = "회원가입"
+            $0.font = .systemFont(ofSize: 17, weight: .semibold)
+        }
+        
+        closeButton.do {
+            $0.tintColor = .black
+            $0.setImage(UIImage(systemName: "xmark"), for: .normal)
+        }
     }
     
     // MARK: - Layout Helper
     
     private func setLayout() {
+        view.addSubviews(topView)
+        topView.addSubviews(topTitleLabel, closeButton)
         
+        topView.snp.makeConstraints {
+            $0.height.equalTo(44)
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        topTitleLabel.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+        
+        closeButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.centerY.equalToSuperview()
+        }
     }
 }
