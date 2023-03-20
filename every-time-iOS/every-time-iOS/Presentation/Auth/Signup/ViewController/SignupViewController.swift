1a7c2d9d819b47a9c9c17f199e78ac2f6159bc72
Mini <minnnidev@gmail.com>
2023-03-20 21:23:42 +0900

    [Feat] #37 - 상단바 기능 구현

--- every-time-iOS/every-time-iOS/Presentation/Auth/Signup/ViewController/SignupViewController.swift
+++ every-time-iOS/every-time-iOS/Presentation/Auth/Signup/ViewController/SignupViewController.swift
@@ -11,6 +11,13 @@ import SnapKit
 import Then
 
 final class SignupViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private let topView: UIView = UIView()
+    private let topTitleLabel: UILabel = UILabel()
+    private let closeButton: UIButton = UIButton()
+    private let backButton: UIButton = UIButton()
 
     // MARK: - View Life Cycle
     
@@ -19,6 +26,7 @@ final class SignupViewController: UIViewController {
 
         setUI()
         setLayout()
+        setAddTarget()
     }
 }
 
@@ -27,12 +35,64 @@ extension SignupViewController {
     // MARK: - UI Components Property
     
     private func setUI() {
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        
+        topTitleLabel.do {
+            $0.text = "회원가입"
+            $0.font = .systemFont(ofSize: 17, weight: .semibold)
+        }
+
+        closeButton.do {
+            $0.tintColor = .black
+            $0.setImage(UIImage(systemName: "xmark"), for: .normal)
+        }
+
+        backButton.do {
+            $0.tintColor = .black
+            $0.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        }
     }
     
     // MARK: - Layout Helper
     
     private func setLayout() {
-        
+        view.addSubviews(topView)
+        topView.addSubviews(topTitleLabel, closeButton, backButton)
+
+        topView.snp.makeConstraints {
+            $0.height.equalTo(44)
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+
+        closeButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.centerY.equalToSuperview()
+        }
+
+        topTitleLabel.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+
+        backButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(15)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Methods
+    
+    private func setAddTarget() {
+        backButton.addTarget(self, action: #selector(backButtonDidTap), for: .touchUpInside)
+        closeButton.addTarget(self, action: #selector(closeButtonDidTap), for: .touchUpInside)
+    }
+    
+    // MARK: - @objc Methods
+    
+    @objc private func backButtonDidTap() {
+        dismiss(animated: false)
+    }
+    
+    @objc private func closeButtonDidTap() {
+        self.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true)
     }
 }
